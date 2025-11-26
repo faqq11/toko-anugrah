729f6a99d3023512f509f40cb908803493fcd7d6
faqq11 <faqihazizy7@gmail.com>
2025-11-26 14:41:24 +0700

    feat: add delete user API

--- server/middlewares/authorization.js
+++ server/middlewares/authorization.js
@@ -1,7 +1,16 @@
 function authorization(req, res, next) {
   try {
     const userData = req.userData;
-    if (userData.role !== "admin") throw new Error("UNAUTHORIZED");
+    const { id } = req.params;
+
+    console.log(userData, id);
+    if (userData.role === "admin") {
+      return next();
+    }
+
+    if (userData.id !== +id) {
+      throw new Error("UNAUTHORIZED");
+    }
 
     next();
   } catch (err) {
